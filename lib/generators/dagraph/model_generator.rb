8aa0614eec40e734e6ee7cdb31f46576b19f782b
Guillaume Dott <guillaume+github@dott.fr>
2025-05-12 11:46:35 +0200

    Improve generator to generate edge model file

--- lib/generators/dagraph/model_generator.rb
+++ lib/generators/dagraph/model_generator.rb
@@ -2,13 +2,25 @@ require 'rails/generators/active_record'
 
 module Dagraph
   module Generators # :nodoc:
-    class MigrationGenerator < Rails::Generators::NamedBase # :nodoc:
+    class ModelGenerator < Rails::Generators::NamedBase # :nodoc:
       include Rails::Generators::Migration
 
       source_root File.expand_path('templates', __dir__)
 
       def self.next_migration_number(number)
         ActiveRecord::Generators::Base.next_migration_number(number)
+      end
+
+      def add_dag_line_to_node_model
+        inject_into_class "app/models/#{klass.model_name.singular}.rb", klass.to_s do
+          "  has_directed_acyclic_graph\n"
+        end
+      end
+      def create_edge_model_file
+        create_file "app/models/#{dagraph.edges_class_name.underscore}.rb", <<~RUBY
+          class #{dagraph.edges_class_name} < ApplicationRecord
+          end
+        RUBY
       end
 
       def create_migration_file
@@ -18,7 +30,7 @@ module Dagraph
       private
 
       def migration_name
-        dagraph.edges_class.table_name
+        dagraph.edges_class_name.underscore.pluralize
       end
 
       def migration_class_name
@@ -30,11 +42,11 @@ module Dagraph
       end
 
       def klass
-        @klass ||= class_name.constantize
+        @klass ||= class_name.camelize.constantize
       end
 
       def dagraph
-        klass._dagraph
+        @dagraph ||= Dagraph::NodeConfig.new(klass)
       end
     end
   end
